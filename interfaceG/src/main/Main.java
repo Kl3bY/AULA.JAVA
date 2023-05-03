098b5fad3468f88a451ab87c2b489a0b6211645e
Kl3bY <125315135+Kl3bY@users.noreply.github.com>
2023-05-02 22:19:06 -0300

    Add files via upload

---
+++ interfaceG/src/main/Main.java
@@ -0,0 +1,15 @@
+
+package main;
+
+import View.Caminhosuave_GUI;
+import View.TELA_GUI;
+
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+        new Caminhosuave_GUI().setVisible(true);
+    }
+    
+}
